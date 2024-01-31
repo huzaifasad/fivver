da3f46f78ea4451f0d32b63d7be6f1167f576934
Huzaifa Saad <140094374+huzaifasad@users.noreply.github.com>
2024-01-31 03:59:43 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,82 +1,3 @@
-// import express from 'express';
-// import mongoose from 'mongoose';
-// import cors from 'cors';
-// import bodyParser from 'body-parser';
-
-// const app = express();
-// const port = 3001;
-// app.use(cors({
-//     origin: "http://localhost:3000", // Update to your frontend URL
-//     methods: ["POST", "GET","PUT","DELETE"],
-//     credentials: true
-//   }));
-// // MongoDB connection
-// app.use(bodyParser.json({ extended: true }));
-// app.use(bodyParser.urlencoded({ extended: true }));
-// const uri = "mongodb+srv://fiverr:12345@cluster0.rxtzhta.mongodb.net/?retryWrites=true&w=majority";
-// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log('Connected to the database'))
-//   .catch(() => console.log('Not connected'));
-
-// // User model
-// const UserSchema = new mongoose.Schema({
-//   firstName: String,
-//   lastName: String,
-//   email: String,
-//   announcementEmail: String,
-//   degrees: String,
-//   password: String,
-// });
-
-// const User = mongoose.model('User', UserSchema);
-
-// app.use(express.json());
-
-// // Register route
-// app.post('/register', async (req, res) => {
-//   const { firstName, lastName, email, announcementEmail, degrees, password } = req.body;
-
-//   try {
-//     // No hashing for simplicity
-//     const newUser = new User({
-//       firstName,
-//       lastName,
-//       email,
-//       announcementEmail,
-//       degrees,
-//       password,
-//     });
-
-//     await newUser.save();
-
-//     res.json({ message: 'User registered successfully.' });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: 'Internal Server Error' });
-//   }
-// });
-
-// // Login route
-// app.post('/login', async (req, res) => {
-//   const { email, password } = req.body;
-
-//   try {
-//     const user = await User.findOne({ email });
-
-//     if (!user || user.password !== password) {
-//       return res.status(401).json({ message: 'Incorrect email or password.' });
-//     }
-
-//     res.json({ message: 'Login successful.' });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: 'Internal Server Error' });
-//   }
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server listening at http://localhost:${port}`);
-// });
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
@@ -96,7 +17,8 @@ const uri = "mongodb+srv://fiverr:12345@cluster0.rxtzhta.mongodb.net/?retryWrite
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 10000, // Set a value that works for your environment
+  serverSelectionTimeoutMS: 30000, // Increased timeout to 30 seconds
+  poolSize: 10, // Increased connection pool size
 })
 .then(() => console.log('Connected to the database'))
 .catch((error) => console.error('Error connecting to the database:', error.message));
@@ -140,9 +62,10 @@ app.post('/register', async (req, res) => {
     }
 });
 
-app.get('/',(req,res)=>{
-    res.send("this works well");
-})
+app.get('/', (req, res) => {
+    res.send("This works well");
+});
+
 // Login route
 app.post('/login', async (req, res) => {
     const { email, password } = req.body;
